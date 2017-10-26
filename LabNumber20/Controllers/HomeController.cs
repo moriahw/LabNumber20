47fcd66ebe974ad2fc0ba195d75113edb4a7cc03
Moriah <moriahkwashington@gmail.com>
2017-10-26 14:58:37 -0400

    This is the first commit

---
+++ LabNumber20/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabNumber20.Models;
+
+namespace LabNumber20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register() //this action shows the registration form
+        {
+            return View();
+        }
+
+        public ActionResult ProcessRegistration(RegisterModel userinput) //this processes the form 
+        {
+            ViewBag.FirstName = userinput.Firstname;
+
+            return View("Confirm");
+        }
+
+        public ActionResult AdminPage()
+        {
+            CoffeeShopDBEntities gccoffeedb = new CoffeeShopDBEntities();
+
+            List<Item> ItemList = gccoffeedb.Items.ToList();
+
+            ViewBag.Details = ItemList;
+
+            return View();
+        }
+
+        public ActionResult NewItem()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewItem(Item AddedItem)
+        {
+            CoffeeShopDBEntities gccoffeedb = new CoffeeShopDBEntities();
+
+            gccoffeedb.Items.Add(AddedItem);
+
+            gccoffeedb.SaveChanges();
+
+            return RedirectToAction("AdminPage");
+        }
+
+        public ActionResult DeleteItem(string ItemName)
+        {
+            CoffeeShopDBEntities gccoffeedb = new CoffeeShopDBEntities();
+            Item Locate = gccoffeedb.Items.Find(ItemName);
+
+            gccoffeedb.Items.Remove(Locate);
+
+            gccoffeedb.SaveChanges();
+
+            return RedirectToAction("AdminPage");
+
+        }
+            
+    }
+}
---
+++ LabNumber20/Models/Register.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabNumber20.Models
+{
+    public class RegisterModel
+    {
+        private string firstname;
+        private string lastname;
+        private string email;
+        private string phonenumber;
+        private string password;
+
+        public string Firstname
+        {
+            set { firstname = value; }
+            get { return firstname; }
+        }
+
+        public string Lastname
+        {
+            set { lastname = value;}
+            get { return lastname; }
+        }
+
+        public string Email
+        {
+            set { email = value; }
+            get { return email; }
+        }
+
+        public string Phonenumber
+        {
+            set { phonenumber = value; }
+            get { return phonenumber; }
+        }
+
+        public string Password
+        {
+            set { password = value; }
+            get { return password; }
+        }
+
+        public RegisterModel()
+        {
+            Firstname = "";
+            Lastname = "";
+            Email = "";
+            Phonenumber = "";
+            Password = "";
+        }
+
+        public RegisterModel(string fn, string ln, string e, string pn, string pw)
+        {
+            Firstname = fn;
+            Lastname = ln;
+            Email = e;
+            Phonenumber = pn;
+            Password = pw;
+        }
+      
+            
+    }
+}
